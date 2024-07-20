0bd2a5f05e483c46a2e0223ffe9f9f9381da586c
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-07-20 22:02:11 +0500

    Update Study.tsx

--- app/components/Study.tsx
+++ app/components/Study.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 // Study Section Funtion
 const Study = () => {
     return (
-        <section className=' w-full mt-40'>
+        <section className=' w-full mt-20 md:mt-40'>
             <div className='p-4 md:pt-4 space-y-8 md:space-y-8'>
                 {/* Heading */}
              <Link href="/Study"> <h1 className="text-green-900 mb-20 font-extrabold text-5xl md:text-7xl text-center">
