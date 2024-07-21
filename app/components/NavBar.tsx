1455734fffd69c20036eb3aa36b1abf12c2d6c91
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-07-21 19:03:27 +0500

    Update NavBar.tsx

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import { HamBurger } from './HamBurgerenu';
+import { HamBurger } from './HamBurgerMenu';
 
 
 // NavBar Component Function
