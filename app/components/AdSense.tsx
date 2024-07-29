0ffdf7faa026e555ce0744438209d2484b82a2af
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-07-30 01:05:15 +0500

    Update AdSense.tsx

--- app/components/AdSense.tsx
+++ app/components/AdSense.tsx
@@ -1,17 +1,12 @@
 import Script from 'next/script';
 import React from 'react'
 
-type AdsenseTypes = {
-    pId: string;
-}
-
-const AdSense = ({ pId }: AdsenseTypes) => {
+const AdSense = () => {
   return (
     <Script
         async
-        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
+        src={"https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7248886941446965"}
         crossOrigin='anonymous'
-        strategy='afterInteractive'
     />
   )
 }
