c9cae89caae91a7f78189db99b5d6be863590a24
syedshahmirsultan <115161841+syedshahmirsultan@users.noreply.github.com>
2024-07-30 00:43:41 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head><meta name="google-adsense-account" content="ca-pub-7248886941446965"></head>
       <body className='overflow-x-hidden'>
-             <meta name="google-adsense-account" content="ca-pub-7248886941446965">
+             
         <Toaster /><NavBar />
         <div className="bg-gray-100/50">{children}</div>
         <Footer />
